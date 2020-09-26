61f53792219876cd6989e0ede6b2172b5aa704bd
Andrew Reynolds <andrew_reynolds@mac.com>
2020-09-25 20:42:26 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const App = () => (
+  <BrowserRouter>
+    <div className="container"></div>
+  </BrowserRouter>
+);
+
+export default App;
